4a8dad2a3558217117bd278b9716dfe9dbec6091
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-09-07 23:46:24 +0530

    commit

--- myApp/ytDownloader/urls.py
+++ myApp/ytDownloader/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('', views.ytd, name="ytd")
+	path('', views.ytd, name="ytd"),
+	path('download/', views.download_page, name="download")
 ]
--- myApp/ytDownloader/views.py
+++ myApp/ytDownloader/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from pytube import YouTube
 
 # Create your views here.
 
@@ -6,4 +7,16 @@ def ytd(request):
 	return render(request, 'ytd.html')
 
 def download_page(request):
-	return render(request, 'download.html')
+	url = request.GET.get('url')
+
+	yt = YouTube(url)
+
+	streams = yt.streams.all()
+
+	res = []
+	for i in streams:
+		res.append(i.resolution)
+
+	return render(request, 'download.html', {
+		'res': res
+	})
