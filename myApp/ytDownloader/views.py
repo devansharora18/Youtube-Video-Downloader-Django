651555e68c982b8683386ec4d602f5b54a84a3ed
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-09-08 13:41:05 +0530

    commit

--- myApp/ytDownloader/views.py
+++ myApp/ytDownloader/views.py
@@ -27,6 +27,9 @@ def download_page(request):
 	if length == 0:
 		length = str(yt.length) + ' seconds'
 
+	thumbnail = yt.thumbnail_url
+	print(thumbnail)
+
 	res = list(dict.fromkeys(res))
 
 	return render(request, 'download.html', {
@@ -34,4 +37,5 @@ def download_page(request):
 		'title': title,
 		'author': author,
 		'length': length,
+		'thumbnail': thumbnail,
 	})
